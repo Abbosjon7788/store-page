7738bd688fdfae90d2bf1743f6b49268d544624d
Abbosjon7788 <nosirovabosjon3429@gmail.com>
2022-04-10 19:41:56 +0500

    failed

--- containers/Navbar/index.jsx
+++ containers/Navbar/index.jsx
@@ -8,8 +8,8 @@ const Navbar = ({ totalPrice }) => {
      const [showDeliveryTime, setShowDeliveryTime] = useState(false)
 
      return (
-          <div className="navbar">
-               <div className="nav-wrapper flex items-center">
+          <div className="navbar flex items-center">
+               <div className="nav-wrapper">
                     <div className="arrow-left pointer flex items-center box-shadow bg-white justify-center radius-10">
                          <Icon icon="arrow-left" width={24} height={24} />
                     </div>
@@ -21,20 +21,20 @@ const Navbar = ({ totalPrice }) => {
                     <div className="search-input bg-white flex items-center radius-10 box-shadow">
                          <Icon icon="search" width={20} height={20} className="light-icon pointer" />
                          <input type="text" placeholder="Search products in Wallmart" className="flex items-center color-black medium-14" />
-                         <Icon icon="filter" width={20} height={20} className="light-icon pointer" />
+                         <Icon icon="filter" width={20} height={20} className="light-icon pointer ml-auto" />
                     </div>
                     <div onClick={() => setShowDeliveryTime(true)} className="time pointer bg-white radius-10 box-shadow">
-                         <div className="time-wrapper flex items-center">
+                         <div className="time-wrapper flex items-center justify-center">
                               <Icon icon="timer" width={20} height={20} />
                               <span className="ml-12 medium-14">By 09:00 — 23:00</span>
                          </div>
                     </div>
-                    <div className="total-price flex items-center bg-white box-shadow radius-10">
-                         <div className="backet-icon flex items-center justify-center">
-                              <Icon icon="backet" width={20} height={20} />
-                         </div>
-                         <span className="ml-12 color-black medium-14">${totalPrice.toFixed(2)}</span>
+               </div>
+               <div className="total-price flex items-center bg-white box-shadow radius-10">
+                    <div className="backet-icon flex items-center justify-center">
+                         <Icon icon="backet" width={20} height={20} />
                     </div>
+                    <span className="ml-12 color-black medium-14">${totalPrice.toFixed(2)}</span>
                </div>
                {showDeliveryTime && <DeliveryTime setShowDeliveryTime={setShowDeliveryTime} />}
           </div>
