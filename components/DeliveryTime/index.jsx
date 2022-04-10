9e5afeaa3b2e11395bad6db110ac8208ec7e56b6
Abbosjon7788 <nosirovabosjon3429@gmail.com>
2022-04-10 11:43:00 +0500

    calendar completed

--- components/DeliveryTime/index.jsx
+++ components/DeliveryTime/index.jsx
@@ -1,9 +1,32 @@
-import { memo, useEffect, useState } from 'react'
+import { memo, useEffect, useState, useMemo } from 'react'
 import Icon from '../../static/Icons'
+import { Calendar } from 'react-multi-date-picker';
+
+const CustomNavigation = ({ direction, handleClick, disabled }) => {
+     return (
+          <div className={`${!disabled && 'hover'} navigation-button`}>
+               {direction === 'right' ? (
+                    <Icon icon="chevron-right"
+                         onClick={handleClick}
+                         className={`${disabled ? 'disabled' : 'cursor-pointer'} icon`}
+                    />
+               ) : (
+                    <Icon icon="chevron-left"
+                         onClick={handleClick}
+                         className="icon cursor-pointer"
+                    />
+               )}
+          </div>
+     );
+};
+
 
 const DeliveryTime = ({ setShowDeliveryTime }) => {
 
      const [show, setShow] = useState(false)
+     let newDate = new Date()
+     let currentDate = newDate.getDate();
+     let currentMonth = newDate.getMonth() + 1;
 
      useEffect(() => {
           setTimeout(() => setShow(true), 1)
@@ -23,6 +46,30 @@ const DeliveryTime = ({ setShowDeliveryTime }) => {
                          <Icon onClick={closeModal} icon="close" width={24} height={24} className="pointer close-icon" />
                     </p>
                     <div className="scroll">
+                         <div className="calendar-wrapper">
+                              <Calendar
+                                   // weekDays={weekDays}
+                                   // months={months}
+                                   showOtherDays={true}
+                                   disableYearPicker={true}
+                                   disableMonthPicker={true}
+                                   readOnly={true}
+                                   weekStartDayIndex={1}
+                                   maxDate={new Date()}
+                                   renderButton={<CustomNavigation />}
+                                   mapDays={props => {
+                                        const { date } = props;
+                                        let color = '';
+                                        if (
+                                             date.day > currentDate + 1 &&
+                                             date.month.number === currentMonth
+                                        )
+                                             color = 'green';
+                                        if (color) return { className: 'highlight-' + color };
+                                   }}
+                              />
+
+                         </div>
                          {
                               [0, 1, 2, 3, 4, 5].map(item => (
                                    <div key={item} className="date flex items-center justify-between radius-10">
